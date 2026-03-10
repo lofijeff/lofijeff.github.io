819c8d4e91aaafe4851f5b3a0df3f6e038df5df4
Eric Östlund <eric.ostlund@proton.me>
2026-03-10 20:48:06 +0100

    qol

--- app.js
+++ app.js
@@ -10,10 +10,22 @@ async function loadPosts() {
 
     const html = marked.parse(md);
 
+    const border1 = document.createElement('div');
+    border1.className = "outer-border";
+
+    const border2 = document.createElement('div');
+    border2.className = "middle-border";
+
+    const border3 = document.createElement('div');
+    border3.className = "inner-border";
+
     const article = document.createElement('article');
     article.innerHTML = html;
 
-    container.appendChild(article);
+    border1.appendChild(border2);
+    border2.appendChild(border3);
+    border3.appendChild(article);
+    container.appendChild(border1);
   }
 }
 
